cb4efbe43d8a6ceaf1a68f88af58145b86aa9a29
lsminter <lsminteris@gmail.com>
2018-06-08 12:12:49 -0700

    added lesson 6 files

--- App/index.js
+++ App/index.js
@@ -2,10 +2,10 @@
 // import App from './react-native-modal';
 // import App from './modal-from-tab-bar';
 // import App from './auth-flow';
-// import App from './redux-integration';
+import App from './redux-integration';
 // import App from './redux-hardware-back-button';
 // import App from './custom-transition';
 // import App from './custom-screen-interpolator';
-import App from './replace-screen';
+// import App from './replace-screen';
 
 export default App;
