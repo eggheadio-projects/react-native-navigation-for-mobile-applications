91c034e4df89b17a2039ab9fbdbf8a158f2c1b28
lsminter <lsminteris@gmail.com>
2018-06-08 12:08:21 -0700

    added lesson 3 files

--- App/react-native-modal/index.js
+++ App/react-native-modal/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, SafeAreaView } from 'react-native';
+import { Button, View, SafeAreaView, Modal, Text, StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 const Home = ({ navigation, screenProps }) => (
@@ -10,7 +10,8 @@ const Home = ({ navigation, screenProps }) => (
     />
     <Button
       title="Go to modal"
-      onPress={() => null}
+      // onPress={() => null}
+      onPress={() => screenProps.changeModalVisibility(true)}
     />
   </SafeAreaView>
 );
@@ -19,7 +20,8 @@ const Details = ({ navigation, screenProps }) => (
   <SafeAreaView>
     <Button
       title="Go to modal"
-      onPress={() => null}
+      // onPress={() => null}
+      onPress={() => screenProps.changeModalVisibility(true)}
     />
   </SafeAreaView>
 );
@@ -39,4 +41,54 @@ const MainAppStack = StackNavigator({
   },
 });
 
-export default MainAppStack;
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.25)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalBody: {
+    backgroundColor: '#fff',
+    width: '80%',
+    padding: 20,
+    borderRadius: 5,
+  },
+});
+
+// NEW CODE BELOW HERE...
+export default class App extends React.Component {
+  state = {
+    modalVisible: false,
+  };
+
+  changeModalVisibility = (modalVisible = false) => {
+    this.setState({ modalVisible });
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <MainAppStack
+          screenProps={{ changeModalVisibility: this.changeModalVisibility }}
+        />
+        <Modal
+          visible={this.state.modalVisible}
+          animationType="fade"
+        >
+          <SafeAreaView style={styles.modalContainer}>
+            <View style={styles.modalBody}>
+              <Text>
+                This is my modal, there are many like it but this one is mine.
+              </Text>
+              <Button
+                title="Close modal"
+                onPress={() => this.changeModalVisibility(false)}
+              />
+            </View>
+          </SafeAreaView>
+        </Modal>
+      </View>
+    );
+  }
+}
