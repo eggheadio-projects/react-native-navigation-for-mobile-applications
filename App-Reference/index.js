24aa2656641b66ce92649fe9ab008fc536cb39d2
ijones16 <ian@egghead.io>
2018-06-19 15:35:38 -0400

    remove App-Reference

--- App-Reference/index.js
+++
@@ -1,9 +0,0 @@
-// import App from './stacknavigator-modal';
-// import App from './react-native-modal';
-import App from './modal-from-tab-bar';
-// import App from './auth-flow';
-// import App from './redux-integration';
-// import App from './custom-transition';
-// import App from './replace-screen';
-
-export default App;
